c7411f93ff2c87adbf049afe6e4c7e455d5c0988
yong.zhang <yong.zhang@HELIX-SG-ZY>
2020-03-24 17:07:48 +0800

    Exception: Application does not exist or is deleted

--- database/toolkit/common_script.sql
+++ database/toolkit/common_script.sql
@@ -1,6 +1,11 @@
+--Exception: Application does not exist or is deleted
+SELECT * FROM Applications 
+WHERE Deleted = 0 
+and Location_Id =(SELECT TOP 1 Location_Id FROM Locations WHERE Location_Is_Here = 1)
 
 select * from locations where Location_Is_Here=1
 --update locations set   Location_Is_Here=0 where Location_Id=10464
+
 
 select * from Cards where Card_Barcode in('9900738890')
 --update Cards set Cash_Balance=0,Bonus_Balance=0,Cumulative_Balance=0,Running_Cumulative_Balance=0 where Card_Barcode in('9900738890')
